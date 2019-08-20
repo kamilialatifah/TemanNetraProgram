3749581920166cb72bb8656c54f4505a1a834969
geyoga <yogadewantama@gmail.com>
2019-08-20 20:58:30 +0700

    Initial Commit

---
+++ TemanNetraProgram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TemanNetraProgram
+//
+//  Created by Georgius Yoga Dewantama on 20/08/19.
+//  Copyright © 2019 Georgius Yoga Dewantama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
