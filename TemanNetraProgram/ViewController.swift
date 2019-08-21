8a1c5b248e2c24205be9a96ef91af107c02c8822
mila <kamilia.latifah@gmail.com>
2019-08-21 09:51:01 +0700

    test

--- TemanNetraProgram/ViewController.swift
+++ TemanNetraProgram/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var test = "hello world"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    
     }
 
 
